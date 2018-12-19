6b371872a4169599b1f1e8d789497c2d8b2ed035
Tomiyzz <1217275317@qq.com>
2018-12-19 21:56:43 +0800

    登录静态资源提交

---
+++ src/components/Login.vue
@@ -0,0 +1,62 @@
+<template>
+  <div class="login">
+
+    <el-form ref="form" :model="form" label-width="80px">
+      <img src="../assets/563253387983844822.jpg" alt="">
+      <el-form-item label="用户名">
+        <el-input v-model="form.username"></el-input>
+      </el-form-item>
+      <el-form-item label="密码">
+        <el-input v-model="form.password"></el-input>
+      </el-form-item>
+      <el-form-item>
+        <el-button type="primary" @click="onSubmit">立即创建</el-button>
+        <el-button>取消</el-button>
+      </el-form-item>
+    </el-form>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      form: {
+        username: '',
+        password: ''
+      }
+    }
+  },
+  methods: {
+    onSubmit () {
+      console.log('submit!')
+    }
+  }
+}
+</script>
+
+<style lang="less">
+  .login {
+    height: 100%;
+    width: 100%;
+    background-color: #2d434c;
+    overflow: hidden;
+    .el-form {
+      width: 400px;
+      background-color: #fff;
+      margin: 200px auto;
+      padding: 75px 40px 15px;
+      position: relative;
+      img {
+        width: 120px;
+        height: 120px;
+        position: absolute;
+        left: 50%;
+        transform: translate(-50%);
+        top: -70px;
+        border-radius: 50%;
+        border: 10px solid #fff;
+      }
+    }
+  }
+</style>
---
+++ src/main.js
@@ -0,0 +1,24 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+// 导入elementui
+import ElementUI from 'element-ui'
+// 导入elementUI得样式
+import 'element-ui/lib/theme-chalk/index.css'
+
+// 导入通用样式
+// import '@/assets/base.less'
+
+// 安装elementUI
+Vue.use(ElementUI)
+
+Vue.config.productionTip = false
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  render: h => h(App)
+})
